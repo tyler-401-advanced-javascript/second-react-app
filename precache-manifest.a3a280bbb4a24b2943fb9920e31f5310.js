758e04fe9df3a97a36693907a63cef17a6d5ea47
Tyler <tylersayvetz@gmail.com>
2020-03-19 20:55:14 -0700

    Updates

---
+++ precache-manifest.a3a280bbb4a24b2943fb9920e31f5310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5912d51c08d82fc655bfb1701ecc04d8",
+    "url": "/second-react-app/index.html"
+  },
+  {
+    "revision": "d0295dcaa2baca00f2a8",
+    "url": "/second-react-app/static/css/main.e677a569.chunk.css"
+  },
+  {
+    "revision": "2ce61252b099061bf594",
+    "url": "/second-react-app/static/js/2.7aa060a9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/second-react-app/static/js/2.7aa060a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0295dcaa2baca00f2a8",
+    "url": "/second-react-app/static/js/main.2c715742.chunk.js"
+  },
+  {
+    "revision": "ff35b31f7ece60fd25a8",
+    "url": "/second-react-app/static/js/runtime-main.94eb709f.js"
+  }
+]);
